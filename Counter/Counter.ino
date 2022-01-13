298b6e31dbb065a1b85946352d07693f58675c21
PMajerczyk <pawel.majerczyk@onet.eu>
2022-01-13 23:24:03 +0100

    update

--- Counter/Counter.ino
+++ Counter/Counter.ino
@@ -1,3 +1,11 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_GrayOLED.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <gfxfont.h>
+
+#include <Adafruit_ILI9341.h>
+
 #include <Adafruit_ILI9341.h>
 #include <Adafruit_GFX.h>
 #include <Wire.h>
--- Display-servo-sensors/Display-servo-sensors.ino
+++ Display-servo-sensors/Display-servo-sensors.ino
@@ -46,7 +46,7 @@ void setup()
   tft.fillScreen(ILI9341_MAGENTA);
   tft.setFont(&FreeSansBold12pt7b);
   tft.setTextSize(2);
-  tft.setRotation(3);
+  tft.setRotation(1);
   tft.setCursor(17, 130);
   tft.println("COVSHIELD");
   delay(3000);
@@ -55,7 +55,7 @@ void setup()
   tft.println("COVSHIELD");
   tft.setCursor(17, 120);
   tft.setTextSize(1);
-  tft.println("Number of people:");
+  tft.println("Number of people: 0");
 
   servo.attach(4);    // Servo set input pin
 
